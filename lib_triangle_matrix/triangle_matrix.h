ce081481f7c12cb8b2b124f94e8c16a6e0b38df5
Юля Забытина <user@mail.ru>
2025-09-18 21:57:42 +0300

    Добавлено объявление класса Triangle Matrix

---
+++ lib_triangle_matrix/triangle_matrix.h
@@ -0,0 +1,145 @@
+#pragma once 
+#include "../lib_matrix/matrix.h"
+template <class T>
+class TriangleMatrix : public Matrix<T> {
+public: 
+    TriangleMatrix();
+    TriangleMatrix(size_t M, size_t N);
+    TriangleMatrix(T* data, size_t M, size_t N);
+    TriangleMatrix(const TriangleMatrix& other);
+    ~TriangleMatrix();
+
+    TriangleMatrix<T> operator + (T value) const;
+    TriangleMatrix<T> operator - (T value) const;
+    TriangleMatrix<T> operator * (T value) const;
+    TriangleMatrix<T> operator / (T value) const;
+
+    TriangleMatrix<T>& operator += (T value);
+    TriangleMatrix<T>& operator -= (T value);
+    TriangleMatrix<T>& operator *= (T value);
+    TriangleMatrix<T>& operator /= (T value);
+
+    TriangleMatrix<T> operator + (const MathVector<T>& vector) const;
+    TriangleMatrix<T> operator - (const MathVector<T>& vector) const;
+    MathVector<T> operator * (const MathVector<T>& vector) const;
+
+    TriangleMatrix<T>& operator += (const MathVector<T>& vector);
+    TriangleMatrix<T>& operator -= (const MathVector<T>& vector);
+
+    TriangleMatrix<T> operator + (const TriangleMatrix<T>& other_matrix) const;
+    TriangleMatrix<T> operator - (const TriangleMatrix<T>& other_matrix) const;
+    TriangleMatrix<T> operator * (const TriangleMatrix<T>& other_matrix) const;
+
+    TriangleMatrix<T>& operator += (const TriangleMatrix<T>& other_matrix);
+    TriangleMatrix<T>& operator -= (const TriangleMatrix<T>& other_matrix);
+    TriangleMatrix<T>& operator *= (const TriangleMatrix<T>& other_matrix);
+
+    friend std::ostream& operator<<(std::ostream& out, const TriangleMatrix<T>& matrix) {
+        for (size_t i = 0; i < matrix.getM(); ++i) {
+            for (size_t j = 0; j < matrix.getN(); ++j) {
+                out << matrix[i][j] << "\t";
+            }
+            out << std::endl;
+        }
+        return out;
+    }
+};
+template <class T>
+TriangleMatrix<T>::TriangleMatrix() : Matrix<T>() {}
+template <class T>
+TriangleMatrix<T>::TriangleMatrix(const TriangleMatrix& other) : Matrix<T>(other) {}
+template <class T>
+TriangleMatrix<T>::~TriangleMatrix() = default;
+
+template <class T>
+TriangleMatrix<T> TriangleMatrix<T>::operator + (T value) const {
+    TriangleMatrix<T> matrix;
+    return matrix;
+}
+template <class T>
+TriangleMatrix<T> TriangleMatrix<T>::operator - (T value) const {
+    TriangleMatrix<T> matrix;
+    return matrix;
+}
+template <class T>
+TriangleMatrix<T> TriangleMatrix<T>::operator * (T value) const {
+    TriangleMatrix<T> matrix;
+    return matrix;
+}
+template <class T>
+TriangleMatrix<T> TriangleMatrix<T>::operator / (T value) const {
+    TriangleMatrix<T> matrix;
+    return matrix;
+}
+
+template <class T>
+TriangleMatrix<T>& TriangleMatrix<T>::operator += (T value) {
+    return *this;
+}
+template <class T>
+TriangleMatrix<T>& TriangleMatrix<T>::operator -= (T value) {
+    return *this;
+
+}
+template <class T>
+TriangleMatrix<T>& TriangleMatrix<T>::operator *= (T value) {
+    return *this;
+}
+template <class T>
+TriangleMatrix<T>& TriangleMatrix<T>::operator /= (T value) {
+    return *this;
+}
+
+template <class T>
+TriangleMatrix<T> TriangleMatrix<T>::operator + (const MathVector<T>& vector) const {
+    TriangleMatrix<T> matrix;
+    return matrix;
+}
+template <class T>
+TriangleMatrix<T> TriangleMatrix<T>::operator - (const MathVector<T>& vector) const {
+    TriangleMatrix<T> matrix;
+    return matrix;
+}
+template <class T>
+MathVector<T> TriangleMatrix<T>::operator * (const MathVector<T>& vector) const {
+    TriangleMatrix<T> matrix;
+    return matrix;
+}
+
+template <class T>
+TriangleMatrix<T>& TriangleMatrix<T>::operator += (const MathVector<T>& vector) {
+    return *this;
+}
+template <class T>
+TriangleMatrix<T>& TriangleMatrix<T>::operator -= (const MathVector<T>& vector) {
+    return *this;
+}
+
+template <class T>
+TriangleMatrix<T> TriangleMatrix<T>::operator + (const TriangleMatrix<T>& other_matrix) const {
+    TriangleMatrix<T> matrix;
+    return matrix;
+}
+template <class T>
+TriangleMatrix<T> TriangleMatrix<T>::operator - (const TriangleMatrix<T>& other_matrix) const {
+    TriangleMatrix<T> matrix;
+    return matrix;
+}
+template <class T>
+TriangleMatrix<T> TriangleMatrix<T>::operator * (const TriangleMatrix<T>& other_matrix) const {
+    TriangleMatrix<T> matrix;
+    return matrix;
+}
+
+template <class T>
+TriangleMatrix<T>& TriangleMatrix<T>::operator += (const TriangleMatrix<T>& other_matrix) {
+    return *this;
+}
+template <class T>
+TriangleMatrix<T>& TriangleMatrix<T>::operator -= (const TriangleMatrix<T>& other_matrix) {
+    return *this;
+}
+template <class T>
+TriangleMatrix<T>& TriangleMatrix<T>::operator *= (const TriangleMatrix<T>& other_matrix) {
+    return *this;
+}
