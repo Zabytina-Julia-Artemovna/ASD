cfd7002e3d94b5b90000f223d86aa54e2fb9cdde
Юля Забытина <user@mail.ru>
2025-09-27 15:48:14 +0300

    Добавлены тесты на операторы *=, /= (вектора на скаляр, в том числе, тест на исключение в случае деления на 0); +, - для двух векторов, оператора * для двух векторов (скалярное произведение)

--- tests/test_math_vector.cpp
+++ tests/test_math_vector.cpp
@@ -72,4 +72,89 @@ TEST(TestMathVectorLib, mathvector_div_on_null_exception) {
     // Assert
     EXPECT_THROW(vector / value, std::logic_error);
 }
+TEST(TestMathVectorLib, mathvector_mult_assign_on_value) {
+    // Arrange & Act
+    int data[3] = { 3, 6, 0 };
+    size_t size = 3;
+    int value = 3;
+    MathVector<int> vector(data, size);
+    vector *= value;
+    // Assert
+    EXPECT_EQ(vector[0], data[0] * value);
+    EXPECT_EQ(vector[1], data[1] * value);
+    EXPECT_EQ(vector[2], data[2] * value);
+}
+TEST(TestMathVectorLib, mathvector_div_assign_on_value) {
+    // Arrange & Act
+    int data[3] = { 4,8,28 };
+    size_t size = 3;
+    int value = 2;
+    MathVector<int> vector(data, size);
+    vector /= value;
+    // Assert
+    EXPECT_EQ(vector[0], data[0] / value);
+    EXPECT_EQ(vector[1], data[1] / value);
+    EXPECT_EQ(vector[2], data[2] / value);
+}
+TEST(TestMathVectorLib, mathvector_div_assign_on_null_exception) {
+    // Arrange & Act
+    int data[3] = { 4,8,28 };
+    size_t size = 3;
+    int value = 0;
+    MathVector<int> vector(data, size);
+    // Assert
+    EXPECT_THROW(vector /= value, std::logic_error);
+}
+TEST(TestMathVectorLib, mathvector_add_vector) {
+    // Arrange & Act
+    int data1[3] = { 4, 7 ,28 };
+    size_t size1 = 3;
 
+    int data2[3] = { 5, 0, 100 };
+    size_t size2 = 3;
+
+    MathVector<int> vector1(data1, size1);
+    MathVector<int> vector2(data2, size2);
+    MathVector<int> result;
+    result = vector1 + vector2;
+    // Assert
+    EXPECT_EQ(result[0], vector1[0] + vector2[0]);
+    EXPECT_EQ(result[1], vector1[1] + vector2[1]);
+    EXPECT_EQ(result[2], vector1[2] + vector2[2]);
+}
+TEST(TestMathVectorLib, mathvector_sub_vector) {
+    // Arrange & Act
+    int data1[3] = { 4, 7 ,28 };
+    size_t size1 = 3;
+
+    int data2[3] = { 5, 0, 100 };
+    size_t size2 = 3;
+
+    MathVector<int> vector1(data1, size1);
+    MathVector<int> vector2(data2, size2);
+    MathVector<int> result;
+    result = vector1 - vector2;
+    // Assert
+    EXPECT_EQ(result[0], vector1[0] - vector2[0]);
+    EXPECT_EQ(result[1], vector1[1] - vector2[1]);
+    EXPECT_EQ(result[2], vector1[2] - vector2[2]);
+}
+TEST(TestMathVectorLib, mathvector_scalar_product) {
+    // Arrange & Act
+    int data1[3] = { 4, 7 ,28 };
+    size_t size1 = 3;
+
+    int data2[3] = { 5, 0, 100 };
+    size_t size2 = 3;
+
+    MathVector<int> vector1(data1, size1);
+    MathVector<int> vector2(data2, size2);
+    int result = 0;
+    int scalar_product =
+        vector1[0] * vector2[0]
+        + vector1[1] * vector2[1]
+        + vector1[2] * vector2[2];
+    result = vector1 * vector2;
+    // Assert
+    EXPECT_EQ(result, scalar_product);  
+}
