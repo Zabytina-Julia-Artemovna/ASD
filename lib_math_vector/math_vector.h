be6bdd48676af9b2ba5bc37f6207a20138e0ab50
Юля Забытина <user@mail.ru>
2025-10-04 11:42:14 +0300

    Добавлен геттер для _start_index и несколько новых конструкторов, учитывающие наличие этого индекса

--- lib_math_vector/math_vector.h
+++ lib_math_vector/math_vector.h
@@ -8,9 +8,16 @@ protected:
 public:
     MathVector();
     MathVector(size_t size);
+    MathVector(size_t size, size_t start_index);
     MathVector(T* data, size_t size);
+    MathVector(T* data, size_t size, size_t start_index);
     MathVector(const MathVector<T>& other);
     virtual ~MathVector();
+
+    size_t get_start_index() const { 
+        return _start_index; 
+    }
+
     MathVector<T> operator * (T value) const;
     MathVector<T> operator / (T value) const;
 
@@ -41,7 +48,12 @@ MathVector<T>::MathVector() : Tvector<T>() {}
 template <class T>
 MathVector<T>::MathVector(size_t size) : Tvector<T>(size) {}
 template <class T>
+MathVector<T>::MathVector(size_t size, size_t start_index) : Tvector<T>(size), _start_index(start_index) {}
+template <class T>
 MathVector<T>::MathVector(T* data, size_t size) : Tvector<T>(data, size) {} 
+template <class T>
+MathVector<T>::MathVector(T* data, size_t size, size_t start_index) : 
+    Tvector<T>(data, size), _start_index(start_index) {}
 template <class T>
 MathVector<T>::MathVector(const MathVector<T>& other) : Tvector<T>(other) {}
 template <class T>
