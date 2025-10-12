8caa1ca6c6145c0b27b90391a9ac49e228167639
Юля Забытина <user@mail.ru>
2025-10-12 13:43:41 +0300

    Добавлены тесты на top() + исключение, на операторы == и =

--- tests/test_stack.cpp
+++ tests/test_stack.cpp
@@ -29,12 +29,6 @@ TEST(TestStackLib, stack_push_element) {
     // Assert
     EXPECT_EQ(stack.top(), element);
 }
-
-
-
-
-
-
 TEST(TestStackLib, stack_push_element_with_exception) {
     // Arrange 
     size_t size = 0;
@@ -60,7 +54,6 @@ TEST(TestStackLib, stack_pop_element) {
     int element1 = 100;
     int element2 = 200;
     int element3 = 300;
-
     Stack<int> stack(size);
     // Act
     stack.push(element1);
@@ -86,7 +79,7 @@ TEST(TestStackLib, stack_is_empty) {
     // Arrange 
     size_t size = 0;
     Stack<int> stack(size);
-    // Assert
+    // Assert & Act
     EXPECT_TRUE(stack.is_empty());
 }
 TEST(TestStackLib, stack_clear) {
@@ -102,4 +95,56 @@ TEST(TestStackLib, stack_clear) {
     stack.clear();
     // Assert
     EXPECT_TRUE(stack.is_empty());
+}
+TEST(TestStackLib, stack_top) {
+    // Arrange 
+    size_t size = 0;
+    int common_element = 100;
+    int last_element = 999;
+    Stack<int> stack(size);
+    // Act
+    for (size_t i = 0; i < 14; ++i) {
+        stack.push(common_element);
+    }
+    stack.push(last_element);
+    // Assert
+    EXPECT_EQ(stack.top(), last_element);
+}
+TEST(TestStackLib, stack_top_with_exception) {
+    // Arrange & Act
+    size_t size = 0;
+    Stack<int> stack(size);
+    // Assert
+    EXPECT_THROW(stack.top(), std::logic_error);
+}
+TEST(TestStackLib, stack_operator_compare_expect_true) {
+    // Arrange & Act
+    size_t size = 10;
+    Stack<int> stack(size);
+    Stack<int> stack_other(size);
+    // Assert
+    EXPECT_TRUE(stack == stack_other);
+}
+TEST(TestStackLib, stack_operator_compare_expect_false) {
+    // Arrange & Act
+    size_t size1 = 10;
+    size_t size2 = 20;
+    Stack<int> stack(size1);
+    Stack<int> stack_other(size2);
+    // Assert
+    EXPECT_FALSE(stack == stack_other);
+}
+TEST(TestStackLib, stack_operator_assign) {
+    //Arrange
+    size_t size1 = 10;
+    size_t size2 = 20;
+    Stack<int> stack(size1);
+    Stack<int> stack_other(size2);
+    //Act
+    stack_other.push(100);
+    stack_other.push(200);
+    stack = stack_other;
+    //Assert
+    EXPECT_TRUE(stack == stack_other);
+    EXPECT_EQ(stack.top(), 200);
 }
