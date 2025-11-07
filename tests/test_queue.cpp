96b11d3552968f1f5744c2d05e95b8611a1ffa66
Юля Забытина <user@mail.ru>
2025-11-07 18:46:44 +0300

    Добавлены тесты на is_empty(), is_full()

--- tests/test_queue.cpp
+++ tests/test_queue.cpp
@@ -28,3 +28,39 @@ TEST(TestQueueLib, queue_copy_constructor) {
         queue2.pop();
     }
 }
+TEST(TestQueueLib, queue_is_empty_true) {
+    // Arrange & Act
+    Queue<int> queue(10);
+    // Assert
+    EXPECT_TRUE(queue.is_empty());
+}
+TEST(TestQueueLib, queue_is_empty_false) {
+    // Arrange
+    size_t size = 3;
+    Queue<int> queue(size);
+    // Act
+    queue.push(10);
+    // Assert
+    EXPECT_FALSE(queue.is_empty());
+}
+TEST(TestQueueLib, queue_is_full_true) {
+    // Arrange
+    size_t size = 3;
+    Queue<int> queue(size);
+    // Act
+    queue.push(30);
+    queue.push(240);
+    queue.push(190);
+    // Assert
+    EXPECT_TRUE(queue.is_full());
+}
+TEST(TestQueueLib, queue_is_full_false) {
+    // Arrange
+    size_t size = 5;
+    Queue<int> queue(size);
+    // Act
+    queue.push(10);
+    queue.push(30);
+    // Assert
+    EXPECT_FALSE(queue.is_full());
+}
