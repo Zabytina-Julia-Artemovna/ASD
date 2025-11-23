f58ffafeee65daa95c2cff641f6e6127700104e2
Юля Забытина <user@mail.ru>
2025-11-23 18:26:40 +0300

    Добавлены тесты на класс DSU

--- tests/test_dsu.cpp
+++ tests/test_dsu.cpp
@@ -26,3 +26,51 @@ TEST(TestDSULib, dsu_copy_constructor) {
         EXPECT_EQ(dsu1.rank()[i], dsu2.rank()[i]);
     }
 }
+TEST(TestDSULib, dsu_union) {
+    // Arrange & Act
+    size_t size = 4;
+    DSU dsu(size);
+    dsu.dsu_union(1, 2);
+    dsu.dsu_union(1, 3);
+    // Assert
+    EXPECT_EQ(dsu.size(), size);
+    EXPECT_EQ(dsu.rank()[1], 1);
+    EXPECT_EQ(dsu.parent()[2], 1);
+    EXPECT_EQ(dsu.parent()[3], 1);
+}
+TEST(TestDSULib, dsu_union_with_exception) {
+    // Arrange & Act
+    size_t size = 5;
+    DSU dsu(size);
+    // Assert
+    ASSERT_THROW(dsu.dsu_union(-1, 2), std::logic_error);
+}
+TEST(TestDSULib, dsu_find_recursive) {
+    // Arrange & Act
+    size_t size = 7;
+    DSU dsu(size);
+    dsu.dsu_union(5, 4);
+    dsu.dsu_union(5, 6);
+    // Assert
+    EXPECT_EQ(dsu.dsu_find_recursive(6), 5);
+    EXPECT_EQ(dsu.dsu_find_recursive(4), 5);
+}
+TEST(TestDSULib, dsu_find_recursive_with_exception) {
+    // Arrange & Act
+    size_t size = 3;
+    DSU dsu(size);
+    // Assert
+    ASSERT_THROW(dsu.dsu_find_recursive(5), std::logic_error);
+}
+TEST(TestDSULib, dsu_operator_assign) {
+    // Arrange & Act
+    size_t size = 3;
+    DSU dsu1(size);
+    DSU dsu2 = dsu1;
+    // Assert
+    EXPECT_EQ(dsu1.size(), dsu2.size());
+    for (size_t i = 0; i < size; i++) {
+        EXPECT_EQ(dsu1.parent()[i], dsu2.parent()[i]);
+        EXPECT_EQ(dsu1.rank()[i], dsu2.rank()[i]);
+    }
+}
