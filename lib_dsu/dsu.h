52db1225a7841f68341e6b5082f8c899a44abee1
Юля Забытина <user@mail.ru>
2025-11-23 11:17:40 +0300

    Добавлено объявление класса DSU и реализация конструкторов

--- lib_dsu/dsu.h
+++ lib_dsu/dsu.h
@@ -2,6 +2,7 @@
 class DSU {
 private:
     int* _parent;
+    int* _rank;
     size_t _size;
 public:
     DSU(size_t size);
@@ -10,3 +11,27 @@ public:
     void dsu_union(int x, int y);
     int dsu_find(int x);
 };
+DSU::DSU(size_t size) {
+    _size = size;
+    _parent = new int[_size];
+    _rank = new int[_size];
+    for (size_t i = 0; i < _size; i++) {
+        _parent[i] = i;
+        _rank[i] = 0;
+    }
+}
+DSU::DSU(const DSU& other) {
+    _size = other._size;
+    _parent = new int[_size];
+    _rank = new int[_size];
+    for (size_t i = 0; i < _size; i++) {
+        this->_parent[i] = other._parent[i];
+        this->_rank[i] = other._rank[i];
+    }
+}
+DSU::~DSU() {
+    delete[] _parent;
+    delete[] _rank;
+    _parent = nullptr;
+    _rank = nullptr;
+}
