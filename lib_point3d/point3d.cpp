39d2d318ac8cd2382898e74b79118ab99a8e8b13
Юля Забытина <user@mail.ru>
2025-11-25 14:22:21 +0300

    Добавлен .cpp файл и cmake лист

---
+++ lib_point3d/point3d.cpp
@@ -0,0 +1,10 @@
+//#include "point3d.h"
+//#include <cmath>
+//
+//int Point3D::getDistanceTo(const Point3D& other) const {
+//    return static_cast<int>(std::round(std::sqrt(
+//        std::pow(other.getX() - _x, 2) +
+//        std::pow(other.getY() - _y, 2) +
+//        std::pow(other.getZ() - _z, 2)
+//    ))); 
+//}
