0821e1ddb6fd9621dd3ac08c30107bf98b1f8eb1
Юля Забытина <user@mail.ru>
2025-11-25 14:23:15 +0300

    Добавлен .cpp файл и cmake лист

---
+++ lib_point/point.cpp
@@ -0,0 +1,8 @@
+//#include "point.h"
+//#include <cmath>
+//int Point::getDistanceTo(const Point& other) const {
+//    return static_cast<int>(std::round(std::sqrt(
+//        std::pow(other.getX() - _x, 2) 
+//        + std::pow(other.getY() - _y, 2
+//        ))));
+//}
