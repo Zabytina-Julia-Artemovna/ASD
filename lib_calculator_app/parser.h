8c35e55a125b5813c18b6318903663362a41ce64
Юля Забытина <user@mail.ru>
2025-11-30 20:56:50 +0300

    Реализация парсера и вспомогательных функций вынесена в .cpp файл

--- lib_calculator_app/parser.h
+++ lib_calculator_app/parser.h
@@ -1,5 +1,19 @@
-#include "lexem.h"
+#pragma once
 #include "../lib_list/List.h"
-namespace Parser {
+#include "lexem.h"
+namespace Parser { 
     List<Lexem> parse(std::string expression);
+    double getSin(double value);
+    double getCos(double value);
+    double getTg(double value);
+    double getAbs(double value);
+    bool isOpenBracket(char c);
+    bool isCloseBracket(char c);
+    bool isDigit(char c);
+    bool isLetter(char c);
+    bool isVariableChar(char c);
+    bool isValidVariableName(const std::string& name);
+    bool isFunction(const std::string& name);
+    bool isBinaryOperator(char c);
+    bool isUnaryOperator(char c, const Lexem* prevLexem);
 }
