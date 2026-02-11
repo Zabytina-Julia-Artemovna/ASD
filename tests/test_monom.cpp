b15773a63c4c34754b552f3acfde75ca9d279ac1
Юля Забытина <user@mail.ru>
2026-02-11 11:27:44 +0300

    Добавлены тесты на проверку подобия мономов и >,<

--- tests/test_monom.cpp
+++ tests/test_monom.cpp
@@ -26,14 +26,70 @@ TEST(TestMonomLib, monom_constructor_with_arguments) {
     EXPECT_EQ(monom.powerZ(), power_z);
 }
 TEST(TestMonomLib, constructor_with_arguments_throws_on_negative_power_x) {
-    ASSERT_THROW(Monom(1.0, -1, 0, 0), std::invalid_argument);
+    ASSERT_THROW(Monom(1.0, -99, 1, 0), std::invalid_argument);
 }
 TEST(TestMonomLib, constructor_with_arguments_throws_on_negative_power_y) {
-    ASSERT_THROW(Monom(1.0, 0, -1, 0), std::invalid_argument);
+    ASSERT_THROW(Monom(10.0, 22, -301, 50), std::invalid_argument);
 }
 TEST(TestMonomLib, constructor_with_arguments_throws_on_negative_power_z) {
-    ASSERT_THROW(Monom(1.0, 0, 0, -1), std::invalid_argument);
+    ASSERT_THROW(Monom(100.5, 33, 67, -100), std::invalid_argument);
 }
 TEST(TestMonomLib, constructor_with_arguments_throws_on_all_negative_powers) {
-    ASSERT_THROW(Monom(1.0, -1, -2, -3), std::invalid_argument);
+    ASSERT_THROW(Monom(39.78, -12, -20, -33), std::invalid_argument);
+}
+TEST(TestMonomLib, is_monoms_similar_true) {
+    // Arrange & Act
+    Monom monom1(10, 2, 3, 4);
+    Monom monom2(34, 2, 3, 4);
+    // Assert
+    EXPECT_TRUE(monom1 == monom2);
+}
+TEST(TestMonomLib, is_monoms_not_similar_true) {
+    // Arrange & Act
+    Monom monom1(10, 2, 29, 4);
+    Monom monom2(34, 2, 3, 4);
+    // Assert
+    EXPECT_TRUE(monom1 != monom2);
+}
+TEST(TestMonomLib, is_monoms_similar_false) {
+    // Arrange & Act
+    Monom monom1(10, 2, 3, 77);
+    Monom monom2(34, 2, 25, 4);
+    // Assert
+    EXPECT_FALSE(monom1 == monom2);
+}
+TEST(TestMonomLib, is_monoms_not_similar_false) {
+    // Arrange & Act
+    Monom monom1(10, 2, 3, 4);
+    Monom monom2(34, 2, 3, 4);
+    // Assert
+    EXPECT_FALSE(monom1 != monom2);
+}
+TEST(TestMonomLib, monom1_greater_monom2_true) {
+    // Arrange & Act
+    Monom monom1(130, 10, 3, 4);
+    Monom monom2(35, 2, 3, 4);
+    // Assert
+    EXPECT_TRUE(monom1 > monom2);
+}
+TEST(TestMonomLib, monom1_less_monom2_true) {
+    // Arrange & Act
+    Monom monom1(10, 1, 1, 2);
+    Monom monom2(34, 2, 3, 4);
+    // Assert
+    EXPECT_TRUE(monom1 < monom2);
+}
+TEST(TestMonomLib, monom1_greater_monom2_false) {
+    // Arrange & Act
+    Monom monom1(130, 10, 3, 4);
+    Monom monom2(35, 2, 3, 389);
+    // Assert
+    EXPECT_FALSE(monom1 > monom2);
+}
+TEST(TestMonomLib, monom1_less_monom2_false) {
+    // Arrange & Act
+    Monom monom1(10, 1, 39, 2);
+    Monom monom2(34, 2, 3, 4);
+    // Assert
+    EXPECT_FALSE(monom1 < monom2);
 }
