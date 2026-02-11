d91568254de5e134ce434089f5afa2792007f0c3
Юля Забытина <user@mail.ru>
2026-02-11 18:34:36 +0300

    Исправлено объявление класса-интерфейса ITable

--- lib_itable/itable.h
+++ lib_itable/itable.h
@@ -2,9 +2,10 @@
 template <class Tkey, class Tvalue>
 class ITable {
 public:
-    virtual void insert(const Tkey& key, const Tvalue& value);
-    virtual void erase(const Tkey& key);
-    virtual Tvalue& found(const Tkey& key) const noexcept;
-    virtual bool is_empty() const noexcept;
-    virtual friend std::ostream& operator << (std::ostream& out, ITable& table);
+    virtual void insert(const Tkey& key, const Tvalue& value) = 0;
+    virtual void erase(const Tkey& key) = 0;
+    virtual Tvalue& found(const Tkey& key) const = 0;
+    virtual bool is_empty() const noexcept = 0;
+    virtual void print(std::ostream& out) const = 0;
+    friend std::ostream& operator << (std::ostream& out, const ITable& table);
 };
