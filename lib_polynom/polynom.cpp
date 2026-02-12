5c6c0a622758bdf3232b74ad23e558104d5fa946
Юля Забытина <user@mail.ru>
2026-02-12 17:05:52 +0300

    Добавлена реализация операторов +, -, * для двух полиномов

--- lib_polynom/polynom.cpp
+++ lib_polynom/polynom.cpp
@@ -1,2 +1,26 @@
 #include "polynom.h"
 const double EPSILON = 1e-10;
+
+Polynom Polynom::operator +(const Polynom& other_polynom) const {
+    Polynom result = *this;
+    for (auto it = other_polynom._polynom.begin(); it != other_polynom._polynom.end(); it++) {
+        result += *it;
+    }
+    return result;
+}
+Polynom Polynom::operator -(const Polynom& other_polynom) const {
+    Polynom result = *this;
+    for (auto it = other_polynom._polynom.begin(); it != other_polynom._polynom.end(); it++) {
+        result -= *it;
+    }
+    return result;
+}
+Polynom Polynom::operator *(const Polynom& other_polynom) const { // x^2, y   // y^2, z
+    Polynom result;
+    for (auto it1 = _polynom.begin(); it1 != _polynom.end(); it1++) {
+        for (auto it2 = other_polynom._polynom.begin(); it2 != other_polynom._polynom.end(); it2++) {
+            result += (*it1) * (*it2);
+        }
+    }
+    return result; 
+}
