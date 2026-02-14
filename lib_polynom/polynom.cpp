38d71d361c119147a0676daf5506be8ed818adb5
Юля Забытина <user@mail.ru>
2026-02-14 15:48:14 +0300

    Добавлена реализация унарного минуса и некоторых арифметических операторов

--- lib_polynom/polynom.cpp
+++ lib_polynom/polynom.cpp
@@ -15,7 +15,7 @@ Polynom Polynom::operator -(const Polynom& other_polynom) const {
     }
     return result;
 }
-Polynom Polynom::operator *(const Polynom& other_polynom) const { // x^2, y   // y^2, z
+Polynom Polynom::operator *(const Polynom& other_polynom) const { 
     Polynom result;
     for (auto it1 = _polynom.begin(); it1 != _polynom.end(); it1++) {
         for (auto it2 = other_polynom._polynom.begin(); it2 != other_polynom._polynom.end(); it2++) {
@@ -23,4 +23,56 @@ Polynom Polynom::operator *(const Polynom& other_polynom) const { // x^2, y   //
         }
     }
     return result; 
+}
+Polynom& Polynom::operator +=(const Polynom& other_polynom) {
+    *this = *this + other_polynom;
+    return *this;
+}
+Polynom& Polynom::operator -=(const Polynom& other_polynom) {
+    *this = *this - other_polynom;
+    return *this;
+}
+Polynom& Polynom::operator *=(const Polynom& other_polynom) {
+    *this = *this * other_polynom;
+    return *this;
+}
+Polynom Polynom::operator +(const Monom& other_monom) const {
+    Polynom result = *this;
+    result += other_monom;
+    result.simplify();
+    result.sort();
+    return result;
+}
+Polynom Polynom::operator -(const Monom& other_monom) const {
+    Polynom result = *this;
+    result -= other_monom;
+    result.simplify();
+    result.sort();
+    return result;
+}
+
+Polynom& Polynom::operator +=(const Monom& other_monom) {
+    *this = *this + other_monom;
+    return *this;
+}
+Polynom& Polynom::operator -=(const Monom& other_monom) {
+    *this = *this - other_monom;
+    return *this;
+}
+Polynom& Polynom::operator=(const Polynom& other) {
+    if (this != &other) {
+        _polynom = other._polynom;
+    }
+    return *this;
+}
+Polynom Polynom::operator-() const {
+    Polynom result;
+    for (auto it = _polynom.begin(); it != _polynom.end(); it++) {
+        result += -(*it);
+    }
+    return result;
+}
+double Polynom::calculate(double x, double y, double z) const {
+    double result;
+
 }
