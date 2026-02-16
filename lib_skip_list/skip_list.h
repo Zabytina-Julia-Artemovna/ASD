a4ad88aefcbf987c29520fa2abda383fca0d0d55
Юля Забытина <user@mail.ru>
2026-02-16 09:44:49 +0300

    Небольшие исправления в объявлении класса список с пропусками

--- lib_skip_list/skip_list.h
+++ lib_skip_list/skip_list.h
@@ -1,22 +1,25 @@
 #pragma once
 #include "../lib_list/List.h"
-template <class Tvalue, class Tkey>
+template <class Tkey, class Tvalue>
 struct Node { //Мн-во указателей на первые узлы на каждом уровне списка
     std::pair <Tkey, Tvalue> data_;
-    Node** next_
+    Node** next_;
+    size_t height_;
 public:
     Node();
     ~Node();
 };
-template <class Tvalue, class Tkey>
+template <class Tkey, class Tvalue>
 class SkipList {
     size_t _max_count_levels;
     size_t _current_count_levels;
     List<Node*> _heads;
 public:
+    SkipList();
+    ~SkipList();
     void insert(const Tkey& key, const Tvalue& value);
     void print() const noexcept;
 protected:
-    size_t flip_coint() const noexcept;
+    size_t flip_coin() const noexcept;
     Node* find_nearest(const Tkey& key) const noexcept;
 };
