6430d3b48e46d6c8756a4e8a4dcabaa5497a2972
Юля Забытина <user@mail.ru>
2026-02-16 10:11:47 +0300

    Небольшие исправления

--- lib_skip_list/skip_list.h
+++ lib_skip_list/skip_list.h
@@ -1,25 +1,25 @@
 #pragma once
 #include "../lib_list/List.h"
 template <class Tkey, class Tvalue>
-struct Node { //Мн-во указателей на первые узлы на каждом уровне списка
+struct SkipListNode { //Мн-во указателей на первые узлы на каждом уровне списка
     std::pair <Tkey, Tvalue> data_;
-    Node** next_;
+    SkipListNode** next_;
     size_t height_;
 public:
-    Node();
-    ~Node();
+    SkipListNode();
+    ~SkipListNode();
 };
 template <class Tkey, class Tvalue>
 class SkipList {
     size_t _max_count_levels;
     size_t _current_count_levels;
-    List<Node*> _heads;
+    List<SkipListNode<Tkey, Tvalue>*> _heads;
 public:
     SkipList();
     ~SkipList();
     void insert(const Tkey& key, const Tvalue& value);
     void print() const noexcept;
 protected:
-    size_t flip_coin() const noexcept;
-    Node* find_nearest(const Tkey& key) const noexcept;
+    size_t flip_coin() const noexcept; //возвращает рандомный уровень для insert нового элемента
+    SkipListNode<Tkey, Tvalue>* find_nearest(const Tkey& key) const noexcept;
 };
