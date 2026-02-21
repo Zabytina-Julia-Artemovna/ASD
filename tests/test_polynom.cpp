ffe5d75a7989be051d934003de2181262a10e6de
Юля Забытина <user@mail.ru>
2026-02-21 16:45:05 +0300

    Добавлены тесты на арифметические операторы и оставшеся функции

--- tests/test_polynom.cpp
+++ tests/test_polynom.cpp
@@ -45,45 +45,211 @@ TEST(TestPolynomLib, polynom_copy_constructor) {
     oss_copy << copy;
     EXPECT_EQ(oss_original.str(), oss_copy.str());
 }
-//TEST(TestPolynomLib, polynom_assignment_operator) {
-//    // Arrange
-//    Monom monom1(10.2, 1, 3, 8);
-//    Monom monom2(5.5, 2, 1, 0);
-//    Polynom original(monom1);
-//    Polynom other(monom2);
-//    // Act
-//    other = original;  // Присваивание существующему объекту
-//    // Assert
-//    EXPECT_EQ(original.size(), other.size());
-//    std::ostringstream oss_original, oss_other;
-//    oss_original << original;
-//    oss_other << other;
-//    EXPECT_EQ(oss_original.str(), oss_other.str());
-//}
-//TEST(TestPolynomLib, polynom_assignment_to_self) {
-//    // Arrange
-//    Monom monom(10.2, 1, 3, 8);
-//    Polynom polynom(monom);
-//    std::ostringstream before;
-//    before << polynom;
-//    // Act
-//    polynom = polynom;  // Присваивание самому себе
-//    // Assert
-//    std::ostringstream after;
-//    after << polynom;
-//    EXPECT_EQ(before.str(), after.str());  // Должен остаться неизменным
-//}
-//TEST(TestPolynomLib, polynom_assignment_deep_copy) {
-//    // Arrange
-//    Polynom p1("2*x*y + 3*z");
-//    Polynom p2("5*x");
-//    // Act
-//    p2 = p1;
-//    // Изменяем оригинал
-//    p1 = Polynom("100*x");  // Создаем новый полином
-//    // Assert
-//    std::ostringstream oss_p2;
-//    oss_p2 << p2;
-//    // p2 должен остаться старым значением ("2*x*y + 3*z")
-//    EXPECT_EQ(oss_p2.str(), "2*x*y + 3*z");
-//}
+TEST(TestPolynomLib, polynom_assignment_operator) {
+    // Arrange
+    Monom monom1(10.2, 1, 3, 8);
+    Monom monom2(5.5, 2, 1, 0);
+    Polynom original(monom1);
+    Polynom other(monom2);
+    // Act
+    other = original;
+    // Assert
+    EXPECT_EQ(original.size(), other.size());
+    std::ostringstream oss_original, oss_other;
+    oss_original << original;
+    oss_other << other;
+    EXPECT_EQ(oss_original.str(), oss_other.str());
+}
+TEST(TestPolynomLib, polynom_self_assignment) {
+    // Arrange
+    Monom monom(10.2, 1, 3, 8);
+    Polynom polynom(monom);
+    std::ostringstream before;
+    before << polynom;
+    // Act
+    polynom = polynom;
+    // Assert
+    std::ostringstream after;
+    after << polynom;
+    EXPECT_EQ(before.str(), after.str()); 
+}
+TEST(TestPolynomLib, polynom_add_polynom) {
+    // Arrange
+    Monom monom1(1.2, 1, 3, 8);
+    Polynom polynom1(monom1);
+    Monom monom2(102.12, 1, 4, 2);
+    Polynom polynom2(monom2);
+    // Act
+    Polynom result;
+    result = polynom1 + polynom2;
+    // Assert
+    EXPECT_EQ(result.size(), 2);
+    std::ostringstream oss;
+    oss << result;
+    EXPECT_EQ(oss.str(), "1.2xy^3z^8 + 102.12xy^4z^2");
+}
+TEST(TestPolynomLib, polynom_sub_polynom) {
+    // Arrange
+    Monom monom1(211.2, 1, 3, 8);
+    Polynom polynom1(monom1);
+    Monom monom2(33.33, 1, 4, 2);
+    Polynom polynom2(monom2);
+    // Act
+    Polynom result;
+    result = polynom1 - polynom2;
+    // Assert
+    EXPECT_EQ(result.size(), 2);
+    std::ostringstream oss;
+    oss << result;
+    EXPECT_EQ(oss.str(), "211.2xy^3z^8 - 33.33xy^4z^2");
+}
+TEST(TestPolynomLib, polynom_mult_polynom) {
+    // Arrange
+    Monom monom1(77.2, 22, 3, 8);
+    Polynom polynom1(monom1);
+    Monom monom2(10, 1, 49, 2);
+    Polynom polynom2(monom2);
+    // Act
+    Polynom result;
+    result = polynom1 * polynom2;
+    // Assert
+    EXPECT_EQ(result.size(), 1);
+    std::ostringstream oss;
+    oss << result;
+    EXPECT_EQ(oss.str(), "772x^23y^52z^10");
+}
+TEST(TestPolynomLib, polynom_add_assign_polynom) {
+    // Arrange
+    Monom monom1(1.2, 1, 3, 8);
+    Polynom polynom1(monom1);
+    Monom monom2(102.12, 1, 4, 2);
+    Polynom polynom2(monom2);
+    // Act
+    polynom1 += polynom2;
+    // Assert
+    EXPECT_EQ(polynom1.size(), 2);
+    std::ostringstream oss;
+    oss << polynom1;
+    EXPECT_EQ(oss.str(), "1.2xy^3z^8 + 102.12xy^4z^2");
+}
+TEST(TestPolynomLib, polynom_sub_assign_polynom) {
+    // Arrange
+    Monom monom1(211.2, 1, 3, 8);
+    Polynom polynom1(monom1);
+    Monom monom2(33.33, 1, 4, 2);
+    Polynom polynom2(monom2);
+    // Act
+    polynom1 -= polynom2;
+    // Assert
+    EXPECT_EQ(polynom1.size(), 2);
+    std::ostringstream oss;
+    oss << polynom1;
+    EXPECT_EQ(oss.str(), "211.2xy^3z^8 - 33.33xy^4z^2");
+}
+TEST(TestPolynomLib, polynom_mult_assign_polynom) {
+    // Arrange
+    Monom monom1(77.2, 22, 3, 8);
+    Polynom polynom1(monom1);
+    Monom monom2(10, 1, 49, 2);
+    Polynom polynom2(monom2);
+    // Act
+    polynom1 *= polynom2;
+    // Assert
+    EXPECT_EQ(polynom1.size(), 1);
+    std::ostringstream oss;
+    oss << polynom1;
+    EXPECT_EQ(oss.str(), "772x^23y^52z^10");
+}
+TEST(TestPolynomLib, polynom_add_monom) {
+    // Arrange
+    Monom monom1(1.2, 4, 4, 8);
+    Polynom polynom(monom1);
+    Monom monom2(66.8, 1, 4, 4);
+    // Act
+    Polynom result;
+    result = polynom + monom2;
+    // Assert
+    EXPECT_EQ(result.size(), 2);
+    std::ostringstream oss;
+    oss << result;
+    EXPECT_EQ(oss.str(), "1.2x^4y^4z^8 + 66.8xy^4z^4");
+}
+TEST(TestPolynomLib, polynom_sub_monom) {
+    // Arrange
+    Monom monom1(2233.4, 1, 2, 8);
+    Polynom polynom(monom1);
+    Monom monom2(99.9, 2, 2, 2);
+    // Act
+    Polynom result;
+    result = polynom - monom2;
+    // Assert
+    EXPECT_EQ(result.size(), 2);
+    std::ostringstream oss;
+    oss << result;
+    EXPECT_EQ(oss.str(), "2233.4xy^2z^8 - 99.9x^2y^2z^2");
+}
+TEST(TestPolynomLib, polynom_add_assign_monom) {
+    // Arrange
+    Monom monom1(1.2, 4, 4, 8);
+    Polynom polynom(monom1);
+    Monom monom2(66.8, 1, 4, 4);
+    // Act
+    polynom += monom2;
+    // Assert
+    EXPECT_EQ(polynom.size(), 2);
+    std::ostringstream oss;
+    oss << polynom;
+    EXPECT_EQ(oss.str(), "1.2x^4y^4z^8 + 66.8xy^4z^4");
+}
+TEST(TestPolynomLib, polynom_sub_assign_monom) {
+    // Arrange
+    Monom monom1(2233.4, 1, 2, 8);
+    Polynom polynom(monom1);
+    Monom monom2(99.9, 2, 2, 2);
+    // Act
+    polynom -= monom2;
+    // Assert
+    EXPECT_EQ(polynom.size(), 2);
+    std::ostringstream oss;
+    oss << polynom;
+    EXPECT_EQ(oss.str(), "2233.4xy^2z^8 - 99.9x^2y^2z^2");
+}
+TEST(TestPolynomLib, polynom_unary_minus) {
+    // Arrange
+    Polynom polynom("22.4xy^2z^8 - 99.9x^2y^2z^2 + 33.7y^3z");
+    // Act
+    polynom = -polynom;
+    // Assert
+    EXPECT_EQ(polynom.size(), 3);
+    std::ostringstream oss;
+    oss << polynom;
+    EXPECT_EQ(oss.str(), "-22.4xy^2z^8 + 99.9x^2y^2z^2 - 33.7y^3z");
+}
+TEST(TestPolynomLib, polynom_calculate) {
+    // Arrange
+    Polynom polynom("2x + 3y - 4z");
+    // Act
+    double result = polynom.calculate(5, 7, 2);
+    // Assert
+    EXPECT_EQ(result, 23);
+}
+TEST(TestPolynomLib, polynom_calculate_very_large) {
+    // Arrange
+    Polynom polynom("10x^5y^3 - 7x^2y^4z + 3yz^5");
+    // Act
+    double result = polynom.calculate(5, 4, 3);
+    // Assert
+    EXPECT_EQ(result, 1868516);
+}
+TEST(TestPolynomLib, polynom_is_zero_expect_true) {
+    // Arrange
+    Polynom polynom;
+    // Act & Assert
+    EXPECT_TRUE(polynom.is_zero());
+}
+TEST(TestPolynomLib, polynom_is_zero_expect_false) {
+    // Arrange
+    Polynom polynom("2x + 32.3y - 4z^5");
+    // Act & Assert
+    EXPECT_FALSE(polynom.is_zero());
+}
