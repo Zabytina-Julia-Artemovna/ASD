afa9cdf5460ac1614a935eeaa16aee374eace269
Юля Забытина <user@mail.ru>
2026-02-24 10:14:57 +0300

    Объявление класса ITable (с небольшими правками)

--- lib_itable/itable.h
+++ lib_itable/itable.h
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <stdexcept>
 template <class Tkey, class Tvalue>
 class ITable {
 public:
+    virtual size_t size() const noexcept = 0;
     virtual void insert(const Tkey& key, const Tvalue& value) = 0;
     virtual void erase(const Tkey& key) = 0;
-    virtual Tvalue& found(const Tkey& key) const = 0;
+    virtual Tvalue& find(const Tkey& key) = 0;
+    virtual const Tvalue& find(const Tkey&) const = 0;
     virtual bool is_empty() const noexcept = 0;
-    virtual void print(std::ostream& out) const = 0;
-    friend std::ostream& operator << (std::ostream& out, const ITable& table);
+    virtual ~ITable() = default;
 };
