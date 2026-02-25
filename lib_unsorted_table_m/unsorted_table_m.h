6eac9a35852fa5d41154f8841ccc9c90112f075c
Юля Забытина <user@mail.ru>
2026-02-25 16:19:26 +0300

    Оценена сложность операций

--- lib_unsorted_table_m/unsorted_table_m.h
+++ lib_unsorted_table_m/unsorted_table_m.h
@@ -22,14 +22,14 @@ public:
     void print(std::ostream& out) const override;
 };
 template <class TKey, class TValue>
-UnsortedTableM<TKey, TValue>& UnsortedTableM<TKey, TValue>::operator=(const UnsortedTableM<TKey, TValue>& other) {
+UnsortedTableM<TKey, TValue>& UnsortedTableM<TKey, TValue>::operator=(const UnsortedTableM<TKey, TValue>& other) { // O(n)
     if (this != &other) {
         _rows = other._rows;
     }
     return *this;
 }
 template <class TKey, class TValue>
-void UnsortedTableM<TKey, TValue>::insert(const TKey& key, const TValue& value) {
+void UnsortedTableM<TKey, TValue>::insert(const TKey& key, const TValue& value) { // O(n)
     for (size_t i = 0; i < _rows.get_size(); i++) {
         if (_rows[i].first == key) {
             throw std::invalid_argument("Key already exists");
@@ -38,15 +38,15 @@ void UnsortedTableM<TKey, TValue>::insert(const TKey& key, const TValue& value) 
     _rows.push_back(std::make_pair(key, value));
 }
 template <class TKey, class TValue>
-size_t UnsortedTableM<TKey, TValue>::size() const noexcept {
+size_t UnsortedTableM<TKey, TValue>::size() const noexcept { // O(1)
     return _rows.get_size();
 }
-template <class TKey, class TValue>
-bool UnsortedTableM<TKey, TValue>::is_empty() const noexcept {
+template <class TKey, class TValue> 
+bool UnsortedTableM<TKey, TValue>::is_empty() const noexcept { // O(1)
     return _rows.is_empty();
 }
 template <class TKey, class TValue>
-void UnsortedTableM<TKey, TValue>::erase(const TKey& key) {
+void UnsortedTableM<TKey, TValue>::erase(const TKey& key) { // O(n)
     for (size_t i = 0; i < _rows.get_size(); i++) {
         if (_rows[i].first == key) {
             _rows.erase(i);
@@ -56,7 +56,7 @@ void UnsortedTableM<TKey, TValue>::erase(const TKey& key) {
     throw std::out_of_range("Key not found");
 }
 template <class TKey, class TValue>
-const TValue& UnsortedTableM<TKey, TValue>::find(const TKey& key) const {
+const TValue& UnsortedTableM<TKey, TValue>::find(const TKey& key) const { // O(n)
     for (size_t i = 0; i < _rows.get_size(); i++) {
         if (_rows[i].first == key) {
             return _rows[i].second;
@@ -65,13 +65,13 @@ const TValue& UnsortedTableM<TKey, TValue>::find(const TKey& key) const {
     throw std::out_of_range("Key not found");
 }
 template <class TKey, class TValue>
-TValue& UnsortedTableM<TKey, TValue>::find(const TKey& key) {
+TValue& UnsortedTableM<TKey, TValue>::find(const TKey& key) { // O(n)
     const UnsortedTableM& const_this = static_cast<const UnsortedTableM&>(*this);
     const TValue& result = const_this.find(key);
     return const_cast<TValue&>(result);
 }
 template <class TKey, class TValue>
-void UnsortedTableM<TKey, TValue>::print(std::ostream& out) const {
+void UnsortedTableM<TKey, TValue>::print(std::ostream& out) const { // O(n)
     out << "Unsorted table on vector: \n";
         for (size_t i = 0; i < _rows.get_size(); i++) {
             out << "| " << _rows[i].first << " | " << _rows[i].second << " |\n";
