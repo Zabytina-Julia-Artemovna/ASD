265ff653060820dcc69e3a71eb6cbf1ae7bd2c22
Юля Забытина <user@mail.ru>
2026-02-25 16:35:09 +0300

    Оценена сложность операций

--- lib_sorted_table_m/sorted_table_m.h
+++ lib_sorted_table_m/sorted_table_m.h
@@ -6,7 +6,7 @@ template <class TKey, class TValue>
 class SortedTableM : public Table<TKey, TValue> {
 private:
     Tvector<std::pair<TKey, TValue>> _rows;
-    std::pair<size_t, bool> find_position(const TKey& key) const {
+    std::pair<size_t, bool> find_position(const TKey& key) const { // O(logn)
         size_t left = 0;
         size_t right = _rows.get_size();
         while (left < right) {
@@ -37,14 +37,14 @@ public:
     void print(std::ostream& out) const override;
 };
 template <class TKey, class TValue>
-SortedTableM<TKey, TValue>& SortedTableM<TKey, TValue>::operator=(const SortedTableM<TKey, TValue>& other) {
+SortedTableM<TKey, TValue>& SortedTableM<TKey, TValue>::operator=(const SortedTableM<TKey, TValue>& other) { // O(n)
     if (this != &other) {
         _rows = other._rows;
     }
     return *this;
 }
 template <class TKey, class TValue>
-void SortedTableM<TKey, TValue>::insert(const TKey& key, const TValue& value) {
+void SortedTableM<TKey, TValue>::insert(const TKey& key, const TValue& value) { // O(n)
     std::pair<size_t, bool> result = find_position(key);
     if (result.second == true) {
         throw std::invalid_argument("Key already exists");
@@ -52,7 +52,7 @@ void SortedTableM<TKey, TValue>::insert(const TKey& key, const TValue& value) {
     _rows.insert(std::make_pair(key, value), result.first);
 }
 template <class TKey, class TValue>
-void SortedTableM<TKey, TValue>::erase(const TKey& key) {
+void SortedTableM<TKey, TValue>::erase(const TKey& key) { // O(logn)
     std::pair<size_t, bool> result = find_position(key);
     if (result.second == false) {
         throw std::out_of_range("Key not found");
@@ -60,13 +60,13 @@ void SortedTableM<TKey, TValue>::erase(const TKey& key) {
     _rows.erase(result.first);
 }
 template <class TKey, class TValue>
-TValue& SortedTableM<TKey, TValue>::find(const TKey& key) {
+TValue& SortedTableM<TKey, TValue>::find(const TKey& key) { // O(logn)
     const SortedTableM& const_this = static_cast<const SortedTableM&>(*this);
     const TValue& result = const_this.find(key);
     return const_cast<TValue&>(result);
 }
 template <class TKey, class TValue>
-const TValue& SortedTableM<TKey, TValue>::find(const TKey& key) const {
+const TValue& SortedTableM<TKey, TValue>::find(const TKey& key) const { // O(logn)
     std::pair<size_t, bool> result = find_position(key);
     if (result.second == false) {
         throw std::out_of_range("Key not found");
@@ -74,15 +74,15 @@ const TValue& SortedTableM<TKey, TValue>::find(const TKey& key) const {
     return _rows[result.first].second;
 }
 template <class TKey, class TValue>
-bool SortedTableM<TKey, TValue>::is_empty() const noexcept {
+bool SortedTableM<TKey, TValue>::is_empty() const noexcept { // O(1)
     return _rows.is_empty();
 }
 template <class TKey, class TValue>
-size_t SortedTableM<TKey, TValue>::size() const noexcept {
+size_t SortedTableM<TKey, TValue>::size() const noexcept { // O(1)
     return _rows.get_size();
 }
 template <class TKey, class TValue>
-void SortedTableM<TKey, TValue>::print(std::ostream& out) const {
+void SortedTableM<TKey, TValue>::print(std::ostream& out) const { // O(n)
     out << "Sorted table on vector: \n";
     for (size_t i = 0; i < _rows.get_size(); i++) {
         out << "| " << _rows[i].first << " | " << _rows[i].second << " |\n";
