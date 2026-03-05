7ac0b1f0e48be50d78466f118d334683b9000b34
Юля Забытина <user@mail.ru>
2026-03-05 11:57:45 +0300

    Арифметические операторы +,-,8 реализованы через +=,-=, *=

--- lib_polynom/polynom.cpp
+++ lib_polynom/polynom.cpp
@@ -22,25 +22,17 @@ Polynom::Polynom(const std::string& str) {
 }
 Polynom Polynom::operator +(const Polynom& other_polynom) const {
     Polynom result = *this;
-    for (auto it = other_polynom._polynom.begin(); it != other_polynom._polynom.end(); it++) {
-        result += *it;
-    }
+    result += other_polynom; 
     return result;
 }
 Polynom Polynom::operator -(const Polynom& other_polynom) const {
     Polynom result = *this;
-    for (auto it = other_polynom._polynom.begin(); it != other_polynom._polynom.end(); it++) {
-        result -= *it;
-    }
+    result -= other_polynom;
     return result;
 }
 Polynom Polynom::operator *(const Polynom& other_polynom) const {
-    Polynom result;
-    for (auto it1 = _polynom.begin(); it1 != _polynom.end(); it1++) {
-        for (auto it2 = other_polynom._polynom.begin(); it2 != other_polynom._polynom.end(); it2++) {
-            result += (*it1) * (*it2);
-        }
-    }
+    Polynom result = *this;
+    result *= other_polynom;
     return result;
 }
 Polynom& Polynom::operator+=(const Polynom& other_polynom) {
