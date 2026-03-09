33dc591ad574ca29b5b250f40a5547d2a6cfe623
Юля Забытина <user@mail.ru>
2026-03-09 16:44:12 +0300

    Добавлено объявление

--- lib_skip_list/skip_list.h
+++ lib_skip_list/skip_list.h
@@ -1,24 +1,28 @@
 #pragma once
-#include "../lib_vector/vector.h"
+#include "../lib_list/List.h"
+#include <utility>
+#include <cstdlib>
 template <class TKey, class TValue>
-struct Node {
+struct TNode {
+    TNode();
+    ~TNode();
     std::pair<TKey, TValue> data_;
-    Node** next_;
-    // конструктор и деструктор?
+    TNode** next_; //указатель на указатель (массив указателей)
 };
 template <class TKey, class TValue>
 class SkipList {
 private:
     size_t _max_levels;
-    size_t _current_levels;
-    Tvector<Node<TKey, TValue>*> _heads;
+    size_t _current_count_levels;
+    List<TNode<TKey, TValue>*> _heads;
 public:
     SkipList();
+    ~SkipList();
     void insert(const TKey& key, const TValue& value);
     void print() const noexcept;
     TValue& find(const TKey& key);
     const TValue& find(const TKey& key) const;
 protected:
     size_t flip_coin() const noexcept;
-    Node* find_nearest(const TKey& key) const noexcept;
+    TNode<TKey, TValue>* find_nearest(const TKey& key) const noexcept;
 };
