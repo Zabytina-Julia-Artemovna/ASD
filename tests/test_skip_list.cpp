56811c322adcc94cd91d9591cc21a0a0e6a63a51
Юля Забытина <user@mail.ru>
2026-03-11 22:07:39 +0300

    Добавлены тесты на Skip List

---
+++ tests/test_skip_list.cpp
@@ -0,0 +1,8 @@
+// Copyright 2025 Julia Zabytina
+#include <stdexcept>
+#include <gtest/gtest.h>
+#include "../lib_skip_list/skip_list.h"
+
+TEST(TestSkipListLib, skip_list_default_constructor) {
+  
+}
