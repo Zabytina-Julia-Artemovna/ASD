e627de491fe6cca12af845778033d6213829e40c
Юля Забытина <user@mail.ru>
2026-05-13 23:28:31 +0300

    Добавлена таблица на AVL-дереве

--- lib_table_avl_tree/table_avl_tree.h
+++ lib_table_avl_tree/table_avl_tree.h
@@ -19,3 +19,52 @@ public:
     size_t size() const noexcept override;
     void print(std::ostream& out) const override;
 };
+template <class TKey, class TValue>
+bool TableAVLTree<TKey, TValue>::is_empty() const noexcept {
+    return _items.is_empty();
+}
+template <class TKey, class TValue>
+size_t TableAVLTree<TKey, TValue>::size() const noexcept {
+    return _size;
+}
+template <class TKey, class TValue>
+TValue& TableAVLTree<TKey, TValue>::find(const TKey& key) {
+    TValue* value = _items.find(key);
+    if (!value) {
+        throw std::invalid_argument("Key not found");
+    }
+    return *value;
+}
+template <class TKey, class TValue>
+const TValue& TableAVLTree<TKey, TValue>::find(const TKey& key) const {
+    TValue* value = _items.find(key);
+    if (!value) {
+        throw std::invalid_argument("Key not found");
+    }
+    return *value;
+}
+template <class TKey, class TValue>
+void TableAVLTree<TKey, TValue>::print(std::ostream& out) const {
+    out << "Sorted table on binary search tree" << "\n";
+    _items.traverse([&out](const std::pair<TKey, TValue>& item) {
+        out << "  " << item.first << " : " << item.second << "\n"; //вывод применяется к каждому эл-ту дерева при обходе
+        });
+}
+template <class TKey, class TValue>
+void TableAVLTree<TKey, TValue>::insert(const TKey& key, const TValue& value) {
+    TValue* node = _items.find(key);
+    if (node) {
+        throw std::invalid_argument("Key already exist");
+    }
+    _items.insert(key, value);
+    _size++;
+}
+template <class TKey, class TValue>
+void TableAVLTree<TKey, TValue>::erase(const TKey& key)  {
+    TValue* node = _items.find(key);
+    if (!node) {
+        throw std::invalid_argument("Key not found");
+    }
+    _items.erase(key);
+    _size--;
+}
