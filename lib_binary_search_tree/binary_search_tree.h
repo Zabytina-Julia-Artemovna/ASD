fd32fbbaca94af8a3b1419aed9d97549322d1693
Юля Забытина <user@mail.ru>
2026-05-17 22:49:58 +0300

    Изменен шаблон на T

--- lib_binary_search_tree/binary_search_tree.h
+++ lib_binary_search_tree/binary_search_tree.h
@@ -2,119 +2,122 @@
 #include <utility>
 #include <iostream>
 #include <stdexcept>
-template <class TKey, class TValue>
+
+template <class T>
 struct NodeBST {
-    NodeBST(const TKey& key, const TValue& value) : data_(key, value), left_(nullptr), right_(nullptr) {}
-    std::pair<TKey, TValue> data_;
-    NodeBST* left_;
-    NodeBST* right_;
+    NodeBST(const T& data) : data_(data), left_(nullptr), right_(nullptr) {}
+    T data_;
+    NodeBST<T>* left_;
+    NodeBST<T>* right_;
 };
-template <class TKey, class TValue>
+
+template <class T>
 class BinarySearchTree {
 private:
-    NodeBST<TKey, TValue>* _root;
+    NodeBST<T>* _root;
+
 public:
     BinarySearchTree();
-    BinarySearchTree(const BinarySearchTree<TKey, TValue>& other);
-    ~BinarySearchTree();   
-    TValue* find(const TKey& key) const noexcept;
-    void insert(const TKey& key, const TValue& value);
-    void erase(const TKey & key);
-    bool is_empty() const noexcept{
+    BinarySearchTree(const BinarySearchTree<T>& other);
+    ~BinarySearchTree();
+
+    typename T::second_type* find(const typename T::first_type& key) const noexcept;
+    void insert(const T& item);  // item - это пара ключ-значение
+    void erase(const typename T::first_type& key);
+
+    bool is_empty() const noexcept {
         return _root == nullptr;
     }
+
     template <class Func>
     void traverse(Func func) const;
     void clear() noexcept;
-    void print_DLCR() const; 
+    void print_DLCR() const;
+
 private:
-    void print_DLCR_recursive(NodeBST<TKey, TValue>* node) const;
-    NodeBST<TKey, TValue>* copy_node(
-        const NodeBST<TKey, TValue>* node) const;
-    void clear_recursive(NodeBST<TKey, TValue>* node) noexcept;
-    NodeBST<TKey, TValue>* find_parent(const TKey& key) const noexcept;
+    void print_DLCR_recursive(NodeBST<T>* node) const;
+    NodeBST<T>* copy_node(NodeBST<T>* node) const;
+    void clear_recursive(NodeBST<T>* node) noexcept;
+    NodeBST<T>* find_parent(const typename T::first_type& key) const noexcept;
+    NodeBST<T>* find_node_by_key(const typename T::first_type& key) const noexcept;
+
     template <class Func>
-    void traverse_recursive(NodeBST<TKey, TValue>* node, Func& func) const;
-    NodeBST<TKey, TValue>* find_max_left(NodeBST<TKey, TValue>* node) const noexcept;
+    void traverse_recursive(NodeBST<T>* node, Func& func) const;
+
+    NodeBST<T>* find_max_left(NodeBST<T>* node) const noexcept;
 };
-template <class TKey, class TValue>
+
+// ==================== Реализации ====================
+
+template <class T>
 template <class Func>
-void BinarySearchTree<TKey, TValue>::traverse_recursive(
-    NodeBST<TKey, TValue>* node,
+void BinarySearchTree<T>::traverse_recursive(
+    NodeBST<T>* node,
     Func& func) const {
     if (!node) return;
-    traverse_recursive(node->left_, func);   
-    func(node->data_);                       
-    traverse_recursive(node->right_, func); 
+    traverse_recursive(node->left_, func);
+    func(node->data_);
+    traverse_recursive(node->right_, func);
 }
-template <class TKey, class TValue>
+
+template <class T>
 template <class Func>
-void BinarySearchTree<TKey, TValue>::traverse(Func func) const {
+void BinarySearchTree<T>::traverse(Func func) const {
     traverse_recursive(_root, func); // рекурсивн. ф-ция обхода дерева (корень, ф-ция котор. примен. к кажд. узлу)
 }
-template <class TKey, class TValue>
-NodeBST<TKey, TValue>* BinarySearchTree<TKey, TValue>::find_max_left(NodeBST<TKey, TValue>* node) const noexcept {
+
+template <class T>
+NodeBST<T>* BinarySearchTree<T>::find_max_left(NodeBST<T>* node) const noexcept {
     // макс элемент среди всех меньших, чем удаляемый узел
 
     if (!node || !node->left_) {
         return nullptr;  // нет левого поддерева нет замены
     }
 
-    NodeBST<TKey, TValue>* current = node->left_;
+    NodeBST<T>* current = node->left_;
 
     // Идём ВПРАВО до конца - к самому большому
     while (current->right_) {
         current = current->right_;
     }
 
-    return current; 
+    return current;
 }
-template <class TKey, class TValue>
-TValue* BinarySearchTree<TKey, TValue>::find(const TKey& key) const noexcept {
-    NodeBST<TKey, TValue>* parent = find_parent(key);
-    if (!parent) {
+
+template <class T>
+typename T::second_type* BinarySearchTree<T>::find(const typename T::first_type& key) const noexcept {
+    NodeBST<T>* node = find_node_by_key(key);
+    if (!node) {
         return nullptr; // дерево пустое - элемент НЕ найден
     }
-    if (parent == _root && _root->data_.first == key) {
-        return &_root->data_.second; // Ключ в корне
-    }
-    if (parent->left_ && parent->left_->data_.first == key) {
-        return &parent->left_->data_.second;
-    }
-    if (parent->right_ && parent->right_->data_.first == key) {
-        return &parent->right_->data_.second;
+    return &node->data_.second;
+}
+
+template <class T>
+void BinarySearchTree<T>::insert(const T& item) {
+    // Проверка на дубликаты ДО вставки
+    if (find_node_by_key(item.first)) {
+        throw std::invalid_argument("Key already exists");
     }
 
-    return nullptr; // дерево не пустое, но такого ключа нет - элемент НЕ найден
-}
-template <class TKey, class TValue>
-void BinarySearchTree<TKey, TValue>::insert(const TKey& key, const TValue& value) {
-    NodeBST<TKey, TValue>* parent = find_parent(key);
+    NodeBST<T>* parent = find_parent(item.first);
     if (!parent) {
-        _root = new NodeBST<TKey, TValue>(key, value);
+        _root = new NodeBST<T>(item);
         return;
     }
 
-    // Проверка на дубликаты перед вставкой
-    if (parent == _root && _root->data_.first == key) {
-        throw std::invalid_argument("Key already exists");
-    }
-    if (parent->left_ && parent->left_->data_.first == key) {
-        throw std::invalid_argument("Key already exists");
-    }
-    if (parent->right_ && parent->right_->data_.first == key) {
-        throw std::invalid_argument("Key already exists");
-    }
+    NodeBST<T>* new_node = new NodeBST<T>(item);
 
-    if (key > parent->data_.first) {
-        parent->right_ = new NodeBST<TKey, TValue>(key, value);
+    if (item.first > parent->data_.first) {
+        parent->right_ = new_node;
     }
     else {
-        parent->left_ = new NodeBST<TKey, TValue>(key, value);
+        parent->left_ = new_node;
     }
 }
-template <class TKey, class TValue>
-NodeBST<TKey, TValue>* BinarySearchTree<TKey, TValue>::find_parent(const TKey& key) const noexcept {
+
+template <class T>
+NodeBST<T>* BinarySearchTree<T>::find_parent(const typename T::first_type& key) const noexcept {
     if (is_empty()) {
         return nullptr;
     }
@@ -123,11 +126,11 @@ NodeBST<TKey, TValue>* BinarySearchTree<TKey, TValue>::find_parent(const TKey& k
         return _root;
     }
 
-    NodeBST<TKey, TValue>* current = _root;
+    NodeBST<T>* current = _root;
     while (current) {
         if (key < current->data_.first) {
             if (!current->left_) {
-                return current; 
+                return current;
             }
             if (current->left_->data_.first == key) {
                 return current;
@@ -136,7 +139,7 @@ NodeBST<TKey, TValue>* BinarySearchTree<TKey, TValue>::find_parent(const TKey& k
         }
         else if (key > current->data_.first) {
             if (!current->right_) {
-                return current;  
+                return current;
             }
             if (current->right_->data_.first == key) {
                 return current;
@@ -148,23 +151,39 @@ NodeBST<TKey, TValue>* BinarySearchTree<TKey, TValue>::find_parent(const TKey& k
         }
     }
     return nullptr;
-}template <class TKey, class TValue>
-NodeBST<TKey, TValue>* BinarySearchTree<TKey, TValue>::copy_node(
-    const NodeBST<TKey, TValue>* node) const {
+}
+
+template <class T>
+NodeBST<T>* BinarySearchTree<T>::find_node_by_key(const typename T::first_type& key) const noexcept {
+    NodeBST<T>* current = _root;
+    while (current) {
+        if (key < current->data_.first) {
+            current = current->left_;
+        }
+        else if (key > current->data_.first) {
+            current = current->right_;
+        }
+        else {
+            return current;
+        }
+    }
+    return nullptr;
+}
+
+template <class T>
+NodeBST<T>* BinarySearchTree<T>::copy_node(NodeBST<T>* node) const {
     if (!node) {
         return nullptr;
     }
-    NodeBST<TKey, TValue>* new_node = new NodeBST<TKey, TValue>(
-        node->data_.first,
-        node->data_.second
-    );
+    NodeBST<T>* new_node = new NodeBST<T>(node->data_);
     new_node->left_ = copy_node(node->left_);
     new_node->right_ = copy_node(node->right_);
 
     return new_node;
 }
-template <class TKey, class TValue>
-void BinarySearchTree<TKey, TValue>::clear_recursive(NodeBST<TKey, TValue>* node) noexcept {
+
+template <class T>
+void BinarySearchTree<T>::clear_recursive(NodeBST<T>* node) noexcept {
     if (node == nullptr) {
         return;
     }
@@ -172,27 +191,32 @@ void BinarySearchTree<TKey, TValue>::clear_recursive(NodeBST<TKey, TValue>* node
     clear_recursive(node->right_);
     delete node;
 }
-template <class TKey, class TValue>
-void BinarySearchTree<TKey, TValue>::clear() noexcept {
+
+template <class T>
+void BinarySearchTree<T>::clear() noexcept {
     clear_recursive(_root);
     _root = nullptr;
 }
-template <class TKey, class TValue>
-BinarySearchTree<TKey, TValue>::BinarySearchTree() {
+
+template <class T>
+BinarySearchTree<T>::BinarySearchTree() {
     _root = nullptr;
 }
-template <class TKey, class TValue>
-BinarySearchTree<TKey, TValue>::BinarySearchTree(const BinarySearchTree<TKey, TValue>& other) {
+
+template <class T>
+BinarySearchTree<T>::BinarySearchTree(const BinarySearchTree<T>& other) {
     _root = copy_node(other._root);
 }
-template <class TKey, class TValue>
-BinarySearchTree<TKey, TValue>::~BinarySearchTree() {
+
+template <class T>
+BinarySearchTree<T>::~BinarySearchTree() {
     clear();
 }
-template <class TKey, class TValue>
-void BinarySearchTree<TKey, TValue>::erase(const TKey& key) {
-    NodeBST<TKey, TValue>* parent = find_parent(key);
-    NodeBST<TKey, TValue>* node = nullptr;
+
+template <class T>
+void BinarySearchTree<T>::erase(const typename T::first_type& key) {
+    NodeBST<T>* parent = find_parent(key);
+    NodeBST<T>* node = nullptr;
     bool is_left_child = false;
 
     if (!parent) {
@@ -234,7 +258,7 @@ void BinarySearchTree<TKey, TValue>::erase(const TKey& key) {
     }
     //  У узла 1 ребенок
     else if (!node->left_) {
-        NodeBST<TKey, TValue>* child = node->right_;
+        NodeBST<T>* child = node->right_;
         if (!parent) {
             _root = child;
         }
@@ -247,7 +271,7 @@ void BinarySearchTree<TKey, TValue>::erase(const TKey& key) {
         delete node;
     }
     else if (!node->right_) {
-        NodeBST<TKey, TValue>* child = node->left_;
+        NodeBST<T>* child = node->left_;
 
         if (!parent) {
             _root = child;
@@ -264,11 +288,11 @@ void BinarySearchTree<TKey, TValue>::erase(const TKey& key) {
     // у узла 2 ребенка
     else {
         // ищем самый правый узел в левом поддереве
-        NodeBST<TKey, TValue>* replacer = find_max_left(node);  
+        NodeBST<T>* replacer = find_max_left(node);
 
         // родитель 
-        NodeBST<TKey, TValue>* replacer_parent = node;
-        NodeBST<TKey, TValue>* temp = node->left_;
+        NodeBST<T>* replacer_parent = node;
+        NodeBST<T>* temp = node->left_;
 
         // вправо до конца запомин родителя
         while (temp->right_) {
@@ -288,17 +312,18 @@ void BinarySearchTree<TKey, TValue>::erase(const TKey& key) {
         delete replacer;
     }
 }
-template <class TKey, class TValue>
-void BinarySearchTree<TKey, TValue>::print_DLCR_recursive(NodeBST<TKey, TValue>* node) const {
+
+template <class T>
+void BinarySearchTree<T>::print_DLCR_recursive(NodeBST<T>* node) const {
     if (node == nullptr) {
         return;
     }
     print_DLCR_recursive(node->left_);
     std::cout << node->data_.second << " ";
     print_DLCR_recursive(node->right_);
-
 }
-template <class TKey, class TValue>
-void BinarySearchTree<TKey, TValue>::print_DLCR() const {
+
+template <class T>
+void BinarySearchTree<T>::print_DLCR() const {
     print_DLCR_recursive(_root);
 }
